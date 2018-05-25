de2f408fa112e1a894e572226f261c021e4faade
Unknown <marcelo.guzman9@yahoo.com>
2018-05-25 01:44:20 -0700

    Part 1:  switch..case, completed codes in js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,30 @@
 let highSchoolGrade = 11;
 let yourGrade;
 
-switch(){
-
-
-
+switch(highSchoolGrade){
+   case 9:
+     yourGrade= 'Freshman';
+     break;
+   case 10:
+     yourGrade= 'Sophomore';
+     break;
+   case 11:
+     yourGrade= 'Junior';
+     break;
+   case 12:
+     yourGrade= 'Senior';
+     break;
+   default:
+     yourGrade = 'Invalid';
 }
+   console.log(yourGrade);
+
+          
+      
+    
+    
+
+
+
+
+
